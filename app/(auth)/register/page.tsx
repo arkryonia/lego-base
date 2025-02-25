aaa9e3806ba02960e291b77e36031a80794a5013
Hodonou Sounton <sounton@gmail.com>
2025-02-25 19:23:58 +0100

    ✨ Auth v0.1.0

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,3 +1,4 @@
+import { signIn } from '@/auth';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
@@ -10,38 +11,10 @@ const Register = () => {
     <div className={'flex min-h-screen justify-center items-center'}>
       <Card className={'w-1/3 p-10'}>
         <CardHeader>
-          <CardTitle className={'text-center text-4xl'}>Create Account</CardTitle>
+          <CardTitle className={'text-center text-4xl'}>Connection</CardTitle>
         </CardHeader>
         <CardContent className={'space-y-6'}>
           <SignInWithGoogle />
-
-          <div className={'flex justify-between h-full items-center text-xl'}>
-            <div className={'w-2/6'}>
-              <Separator />
-            </div>
-            <div className={'w-2/3 text-center'}> Or continue with email</div>
-            <div className={'w-2/6'}>
-              <Separator />
-            </div>
-          </div>
-          <form className={'space-y-6'}>
-            <div>
-              <label htmlFor="email"></label>
-              <Input type="email" className={'p-7'} placeholder={'Email'} />
-            </div>
-            <div>
-              <label htmlFor="password"></label>
-              <Input type="password" className={'p-7'} placeholder={'Password'} />
-            </div>
-            <Button className={'p-7 w-full text-xl'}>Sign Up</Button>
-          </form>
-
-          <p className={'text-center'}>
-            Already have an account ?{' '}
-            <Link href={'/login'} className={'text-blue-400 font-bold'}>
-              Sign In
-            </Link>
-          </p>
         </CardContent>
       </Card>
     </div>
@@ -49,3 +22,55 @@ const Register = () => {
 };
 
 export default Register;
+
+function RegisterForm() {
+  return (
+    <>
+      <div className={'flex justify-between h-full items-center text-xl'}>
+        <div className={'w-2/6'}>
+          <Separator />
+        </div>
+        <div className={'w-2/3 text-center'}> Or continue with email</div>
+        <div className={'w-2/6'}>
+          <Separator />
+        </div>
+      </div>
+      <form
+        className={'space-y-6'}
+        action={async formData => {
+          'use server';
+          await signIn('credentials', formData);
+        }}
+      >
+        <div>
+          <label htmlFor="email"></label>
+          <Input name="email" id="email" type="email" className={'p-7'} placeholder={'Email'} />
+        </div>
+        <div>
+          <label htmlFor="password"></label>
+          <Input
+            name="password"
+            id="password"
+            type="password"
+            className={'p-7'}
+            placeholder={'Password'}
+          />
+        </div>
+        <Button type="submit" className={'p-7 w-full text-xl'}>
+          Sign Up
+        </Button>
+      </form>
+    </>
+  );
+}
+
+function AlreadyExitButton() {
+  return (
+    <p className={'text-center'}>
+      Already have an account ?{' '}
+      <Link href={'/login'} className={'text-blue-400 font-bold'}>
+        Sign In
+      </Link>
+    </p>
+  );
+}
--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,11 @@
 // noinspection JSUnusedGlobalSymbols
 
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google";
-import {prisma} from "../prisma/prisma";
-import {PrismaAdapter} from "@auth/prisma-adapter";
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+import { prisma } from '../prisma/prisma';
 
-export const {handlers, signIn, signOut, auth} = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [Google],
-})
-
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [Google],
+});
